bdf90d29471b7f52b481a155256cfc19537e2657
LQMemer <42935807+MemerRedeemer@users.noreply.github.com>
2022-04-13 14:46:11 +0200

    Pickup

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -8,40 +8,47 @@ public class PickUp:MonoBehaviour {
     public float rayDistance = 1.5f, placeDistance = 2;
     GameObject savePickup;
     int saveInventoryNumber;
-    bool hasPickup = false;
+    bool hasPickup, canPickup = true;
 
     void Update() {
-
         if(Input.GetKeyDown(KeyCode.E) && hasPickup) {
-            PlacePickup();
+            PlaceItem();
         }
 
-        if(Input.GetKeyDown(KeyCode.E) && !hasPickup) {
+        if(Input.GetKeyDown(KeyCode.E) && !hasPickup && canPickup) {
             CheckPickup();
         }
     }
 
-    void PlacePickup() {
+    void PlaceItem() {
         //Add raycast too make sure the item can be placed
-        Instantiate(savePickup, transform.position + (transform.forward * placeDistance), Quaternion.identity);
+        Instantiate(prefabsInventory[saveInventoryNumber], transform.position + (transform.forward * placeDistance), Quaternion.identity);
         inventory[saveInventoryNumber].SetActive(false);
         hasPickup = false;
+        Invoke(nameof(CanPickup), 0.025f);
     }
 
     void CheckPickup() {
         RaycastHit hit;
         if(Physics.Raycast(transform.position, transform.forward * rayDistance, out hit, rayDistance)) {
-            if(hit.collider.tag == "Pickup") {
-                for(int i = 0; i < inventory.Length; i++) {
-                    if(hit.collider.name == inventory[i].name) {
-                        inventory[i].gameObject.SetActive(true);
-                        savePickup = inventory[i];
-                        saveInventoryNumber = i;
-                        hasPickup = true;
-                        Destroy(hit.collider.gameObject);
-                    }
-                }
+            switch(hit.collider.tag) {
+                case "Key":
+                    savePickup = hit.collider.gameObject;
+                    PickInventory(0);
+                    break;
             }
         }
+    }
+
+    void PickInventory(int selectedNumber) {
+        inventory[selectedNumber].SetActive(true);
+        saveInventoryNumber = selectedNumber;
+        hasPickup = true;
+        canPickup = false;
+        Destroy(savePickup);
+    }
+
+    void CanPickup() {
+        canPickup = true;
     }
 }

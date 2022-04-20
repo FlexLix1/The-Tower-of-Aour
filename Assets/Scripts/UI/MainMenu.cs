a225e9ba157f07fd3ce7e0dec89fd2c9a6ec0b9e
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-04-20 15:02:41 +0200

    Added pauseMenu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    private PauseMenu pause;
+
+    private void Start()
+    {
+        pause = GetComponent<PauseMenu>();
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void backToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+
+    public void BackInGame()
+    {
+        pause.PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        pause.GameIsPaused = false;
     }
     public void Quit()
     {

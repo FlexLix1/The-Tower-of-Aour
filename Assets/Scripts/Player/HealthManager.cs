1f96ede2aabda14a19c63f77c909d6c092c1f261
FlexLix1 <90191930+FlexLix1@users.noreply.github.com>
2022-04-21 14:46:39 +0200

    pyttesmå ändringar gällande checkpoints

--- Assets/Scripts/Player/HealthManager.cs
+++ Assets/Scripts/Player/HealthManager.cs
@@ -11,14 +11,22 @@ public class HealthManager : MonoBehaviour {
     private bool isRespawning;
     private Vector3 respawnPoint;
     public float respawnTime;
+    public int damage = 1;
 
     void Start() {
         currentHealth = maxHealth;
 
         respawnPoint = player.transform.position;
     }
+
+    private void Update() {
+        if (Input.GetKeyDown("p")) {
+            HurtPlayer(damage);
+        }
+    }
     public void HurtPlayer(int damage) {
         currentHealth -= damage;
+
         if (currentHealth <= 0) {
             Respawn();
         }
@@ -42,6 +50,5 @@ public class HealthManager : MonoBehaviour {
 
     public void SetSpawnPoint(Vector3 newPos) {
         respawnPoint = newPos;
-
     }
 }

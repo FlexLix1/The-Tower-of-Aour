1287ced93ba313aea0381de8ca03dae31babf5e9
Dannyedmalm <danedm1030@skola.goteborg.se>
2022-04-26 14:00:22 +0200

    Revert "Merge branch 'main' into Dannys-branch"
    This reverts commit 12766c9e391f6a4af7bf9ed6b1fd4da1f69b7735, reversing
    changes made to 1c6872c162dfe4129e469f1b7d50502a18540ae6.

--- Assets/Scripts/Player/Interact.cs
+++ Assets/Scripts/Player/Interact.cs
@@ -4,43 +4,27 @@ using UnityEngine;
 
 public class Interact:MonoBehaviour {
 
-    SimpleLever leverScript;
-    string saveTag;
-    bool onTrigger;
+    float rayDistance = 1.5f;
 
     void Update() {
-        if(!onTrigger)
-            return;
-
         if(Input.GetKeyDown(KeyCode.E)) {
-            Activate();
+            CheckInteraction();
         }
     }
 
-    void Activate() {
-        switch(saveTag) {
-            case "Lever":
-                if(leverScript.leverActive) {
-                    leverScript.LeverDown();
-                } else {
-                    leverScript.LeverUp();
-                }
-                break;
+    void CheckInteraction() {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward * rayDistance, out hit, rayDistance)) {
+            switch(hit.collider.tag) {
+                case "Lever":
+                    SimpleLever leverScript = hit.collider.gameObject.GetComponent<SimpleLever>();
+                    if(leverScript.leverActive) {
+                        leverScript.LeverDown();
+                    } else {
+                        leverScript.LeverUp();
+                    }
+                    break;
+            }
         }
-    }
-
-    void OnTriggerEnter(Collider other) {
-        switch(other.tag) {
-            case "Lever":
-                onTrigger = true;
-                saveTag = other.tag;
-                leverScript = other.gameObject.GetComponent<SimpleLever>();
-                break;
-        }
-    }
-
-    void OnTriggerExit(Collider other) {
-        if(onTrigger)
-            onTrigger = false;
     }
 }

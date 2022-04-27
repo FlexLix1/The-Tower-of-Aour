423c790523bf3086e4e8af8bd8baec183e14a7cf
LQMemer <42935807+MemerRedeemer@users.noreply.github.com>
2022-04-27 11:44:25 +0200

    Updated my own scene

--- Assets/Scripts/DynamicDoor.cs
+++ Assets/Scripts/DynamicDoor.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class DynamicDoor:MonoBehaviour {
 
+    Rigidbody rgbd;
+
+    public float speed = 3;
+
     public Vector3 moveTowards;
     public bool swiningDoor, openDoor;
     bool moving;
 
     void Start() {
-
+        rgbd = GetComponent<Rigidbody>();
     }
 
     void Update() {
 
+    }
+
+    void MoveDoorTo(Vector3 target) {
+        Vector3 forcedDirection = target - transform.position;
+        Vector3.Normalize(forcedDirection);
+        rgbd.velocity = forcedDirection * speed;
     }
 }

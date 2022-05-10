495d257fb9ce81d2c17502c004fe1c9bdc79bd37
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-05-10 14:28:14 +0200

    Fixes on level 1

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -16,7 +16,7 @@ public class Elevator:MonoBehaviour {
         elevatorStart = transform.position;
         elevatorTarget += elevatorStart;
     }
-
+    
     void Update() {
 
         if(!elevatorActive) {

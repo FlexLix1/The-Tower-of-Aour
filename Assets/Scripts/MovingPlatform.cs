c9ab660e439b6b414bb34e693ddb5d616bdc09d6
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-05-10 15:53:31 +0200

    Added fence and added colliders to them + moving platforms

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,50 +4,24 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    [SerializeField] Vector3 platformTarget;
+    [SerializeField] float distanceToCover;
+    [SerializeField] float speed;
+    
 
-    public bool platFormActive;
-    public float speed;
-
-    Vector3 platformStart;
-
-    Rigidbody rgbd;
+    Vector3 startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        rgbd = GetComponent<Rigidbody>();
-        platformStart = transform.position;
-        platformTarget += platformStart;
-        platFormActive = true;
+        startPosition = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (platFormActive)
-        {
-            if (Vector3.Distance(platformStart, transform.position) > 0.005f)
-            {
-                MovePlatFormTo(platformStart);
-                platFormActive = false;
-            }
-        }
-
-        if (!platFormActive)
-        {
-            if (Vector3.Distance(platformTarget, transform.position) > 0.005f)
-            {
-                MovePlatFormTo(platformTarget);
-                platFormActive = true; ;
-            }
-        }
-
-        void MovePlatFormTo(Vector3 target)
-        {
-            Vector3 forcedDirection = target - transform.position;
-            Vector3.Normalize(forcedDirection);
-            rgbd.velocity = forcedDirection * speed;
-        }
+        Vector3 Velocity = startPosition;
+        Velocity.x += distanceToCover * Mathf.Sin(Time.time * speed);
+        transform.position = Velocity;
     }
 }

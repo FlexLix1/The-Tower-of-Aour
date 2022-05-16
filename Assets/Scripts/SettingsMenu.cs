91bbbe2df42ecb7e6c67d8a09c96f96cf23ed90f
FlexLix1 <90191930+FlexLix1@users.noreply.github.com>
2022-05-16 11:20:37 +0200

    main menu options done

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,11 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro;
 
-public class SettingsMenu : MonoBehaviour
-{
+public class SettingsMenu : MonoBehaviour {
+    Resolution[] resolutions;
+    public TMPro.TMP_Dropdown resolutionDropdown;
     public AudioMixer audioMixer;
+
+    private void Start() {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++) {
+            string option = resolutions[i].width + "x" + resolutions[i].height + "@" + resolutions[i].refreshRate + "hz";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height) {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     public void SetVolume(float volume) {
         audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetQuailty(int qualityIndex) {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullscreen) {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex) {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }

fe08b35f4c367d4b465a93c6dc2376188d3f977b
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-05-18 15:48:39 +0200

    updates on level 1 and also spinscript

--- Assets/Scripts/MovingObjects/spinscript.cs
+++ Assets/Scripts/MovingObjects/spinscript.cs
@@ -4,18 +4,10 @@ using UnityEngine;
 
 public class spinscript : MonoBehaviour
 {
-    public float speedX;
-    public float speedY;
-    public float speedZ;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speedX, speedY, speedZ;
     void Update()
     {
         transform.Rotate(speedX, speedY, speedZ, Space.World);
     }
+    
 }

817020e32cebdbbaeb879c4d5af2187a1f5200d1
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-05-19 17:31:52 +0200

    updates

--- Assets/Scripts/Camera Scripts/CameraShake.cs
+++ Assets/Scripts/Camera Scripts/CameraShake.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-namespace UnityCore{
+namespace UnityCore
+{
     namespace Audio
     {
         public class CameraShake : MonoBehaviour
         {
             public float delayBetweenCameraShake;
+            public ParticleSystem bricks;
+            public ParticleSystem dust;
+            public ParticleSystem cogwheel;
+
+            public float offsetFromPlayer;
+
+            Vector3 followPlayer;
+            GameObject player;
+
             CinemachineImpulseSource impulse;
             AudioController audioController;
             private bool shakeIsTrue;
@@ -17,15 +27,17 @@ namespace UnityCore{
             {
                 audioController = GameObject.Find("AudioController").GetComponent<AudioController>();
                 impulse = transform.GetComponent<CinemachineImpulseSource>();
-
                 shakeIsTrue = true;
+                player = GameObject.FindGameObjectWithTag("Player");
             }
 
             // Update is called once per frame
             void Update()
             {
+
                 if (shakeIsTrue)
                 {
+                    followPlayer = player.transform.position + player.transform.up * offsetFromPlayer;
                     Invoke("shake", delayBetweenCameraShake);
                     shakeIsTrue = false;
                 }
@@ -33,11 +45,31 @@ namespace UnityCore{
 
             void shake()
             {
+                bricks = Instantiate(bricks, followPlayer, transform.rotation);
+                cogwheel = Instantiate(cogwheel, followPlayer, transform.rotation);
+                dust = Instantiate(dust, followPlayer, transform.rotation);
                 impulse.GenerateImpulse();
                 shakeIsTrue = true;
                 audioController.PlayAudio(AudioType.SFX_BellSound, true);
+                activateParticles();
             }
+
+            void activateParticles()
+            {
+
+                bricks.Play();
+                cogwheel.Play();
+                dust.Play();
+                Invoke(nameof(stopParticles), 3);
+            }
+
+            void stopParticles()
+            {
+                bricks.Stop();
+                dust.Stop();
+                cogwheel.Stop();
+            }
+
         }
     }
 }
-  

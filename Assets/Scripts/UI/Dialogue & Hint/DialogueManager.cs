64e0d5f753300c443969e371b1e6fe7b39909e45
LQMemer <42935807+MemerRedeemer@users.noreply.github.com>
2022-05-19 06:33:41 +0200

    Fixes!

--- Assets/Scripts/UI/Dialogue & Hint/DialogueManager.cs
+++ Assets/Scripts/UI/Dialogue & Hint/DialogueManager.cs
@@ -2,94 +2,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-namespace UnityCore {
-    namespace Audio {
-        public class DialogueManager : MonoBehaviour {
-            [SerializeField] GameObject dialogueBox, nextDialogueArrow;
-            [SerializeField] Text nameText, dialogueText;
-            AudioController audioController;
-            DialogueHolder getDialogue;
 
-            public float textSpeed = 0.025f;
+public class DialogueManager:MonoBehaviour {
+    [SerializeField] GameObject dialogueBox, nextDialogueArrow;
+    [SerializeField] Text nameText, dialogueText;
+    DialogueHolder getDialogue;
 
-            string[] holdDialogue;
-            bool dialogueActive, writingOut;
-            int numInDialogue;
+    public AudioClip nextSentence;
+    AudioSource audioSource;
 
-            void Start() {
-                audioController = GameObject.Find("AudioController").GetComponent<AudioController>();
-            }
+    public float textSpeed = 0.025f;
 
-            void Update() {
-                if (!dialogueActive)
-                    return;
+    string[] holdDialogue;
+    bool dialogueActive, writingOut;
+    int numInDialogue;
 
-                if (writingOut && Input.GetKeyDown(KeyCode.E)) {
-                    SkipSentence();
-                    return;
-                }
+    void Start() {
+        audioSource = GetComponent<AudioSource>();    
+    }
 
-                if (Input.GetKeyDown(KeyCode.E)) {
-                    NextSentence();
-                    audioController.PlayAudio(AudioType.SFX_ClickInTextBox);
-                }
-            }
+    void Update() {
+        if(!dialogueActive)
+            return;
 
-            void SkipSentence() {
-                StopAllCoroutines();
-                dialogueText.text = holdDialogue[numInDialogue];
-                nextDialogueArrow.SetActive(true);
-                writingOut = false;
-                numInDialogue++;
-            }
-
-            void NextSentence() {
-                if (numInDialogue == getDialogue.dialogueText.Length) {
-                    nextDialogueArrow.SetActive(false);
-                    getDialogue.dialogueDone = true;
-                    dialogueActive = false;
-                    dialogueBox.SetActive(false);
-                    numInDialogue = 0;
-                    return;
-                }
-
-                nextDialogueArrow.SetActive(false);
-                StartCoroutine(TypeSentence(holdDialogue[numInDialogue]));
-            }
-
-            void OnTriggerEnter(Collider other) {
-                if (dialogueActive)
-                    return;
-
-                if (other.CompareTag("Dialogue")) {
-                    getDialogue = other.GetComponent<DialogueHolder>();
-                    if (getDialogue.dialogueDone)
-                        return;
-
-                    dialogueActive = true;
-                    dialogueBox.SetActive(true);
-                    nameText.text = getDialogue.speakerName + ":";
-                    holdDialogue = new string[getDialogue.dialogueText.Length];
-                    for (int i = 0; i < holdDialogue.Length; i++) {
-                        holdDialogue[i] = getDialogue.dialogueText[i];
-                    }
-
-                    StartCoroutine(TypeSentence(holdDialogue[numInDialogue]));
-                }
-            }
-
-            IEnumerator TypeSentence(string sentence) {
-                dialogueText.text = "";
-                writingOut = true;
-                foreach (char letter in sentence.ToCharArray()) {
-                    dialogueText.text += letter;
-                    yield return new WaitForSeconds(textSpeed);
-                }
-                writingOut = false;
-                nextDialogueArrow.SetActive(true);
-                StopCoroutine(TypeSentence(holdDialogue[numInDialogue]));
-                numInDialogue++;
-            }
+        if(writingOut && Input.GetKeyDown(KeyCode.E)) {
+            SkipSentence();
+            return;
         }
+
+        if(Input.GetKeyDown(KeyCode.E)) {
+            NextSentence();
+            audioSource.PlayOneShot(nextSentence);
+        }
+    }
+
+    void SkipSentence() {
+        StopCoroutine(TypeSentence(holdDialogue[numInDialogue]));
+        dialogueText.text = holdDialogue[numInDialogue];
+        nextDialogueArrow.SetActive(true);
+        writingOut = false;
+        numInDialogue++;
+    }
+
+    void NextSentence() {
+        if(numInDialogue == getDialogue.dialogueText.Length) {
+            nextDialogueArrow.SetActive(false);
+            getDialogue.dialogueDone = true;
+            dialogueActive = false;
+            dialogueBox.SetActive(false);
+            numInDialogue = 0;
+            return;
+        }
+
+        nextDialogueArrow.SetActive(false);
+        StartCoroutine(TypeSentence(holdDialogue[numInDialogue]));
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(dialogueActive)
+            return;
+
+        if(other.CompareTag("Dialogue")) {
+            getDialogue = other.GetComponent<DialogueHolder>();
+            if(getDialogue.dialogueDone)
+                return;
+
+            dialogueActive = true;
+            dialogueBox.SetActive(true);
+            nameText.text = getDialogue.speakerName + ":";
+            holdDialogue = new string[getDialogue.dialogueText.Length];
+            for(int i = 0; i < holdDialogue.Length; i++) {
+                holdDialogue[i] = getDialogue.dialogueText[i];
+            }
+
+            StartCoroutine(TypeSentence(holdDialogue[numInDialogue]));
+        }
+    }
+
+    IEnumerator TypeSentence(string sentence) {
+        dialogueText.text = "";
+        writingOut = true;
+        foreach(char letter in sentence.ToCharArray()) {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(textSpeed);
+        }
+        writingOut = false;
+        nextDialogueArrow.SetActive(true);
+        numInDialogue++;
     }
 }

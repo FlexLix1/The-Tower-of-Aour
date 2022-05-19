4b977d7a1d1d408c8a5339a1134d7815fc825ba3
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-05-19 11:34:16 +0200

    fixed a camera shake

--- Assets/Scripts/Camera Scripts/CameraShake.cs
+++ Assets/Scripts/Camera Scripts/CameraShake.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraShake : MonoBehaviour
 {
+    private bool shakeIsTrue;
+    CinemachineImpulseSource impulse;
     // Start is called before the first frame update
     void Start()
     {
-        
+        impulse = transform.GetComponent<CinemachineImpulseSource>();
+
+        shakeIsTrue = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (shakeIsTrue)
+        {
+            Invoke("shake", 3f);
+            shakeIsTrue = false;
+        }
+    }
+
+    void shake()
+    {
+        impulse.GenerateImpulse();
+        shakeIsTrue = true;
         
     }
 }
+  

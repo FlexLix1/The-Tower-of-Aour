1548bfc342fda14450985d099fe2b4d8f440e1f2
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-05-20 00:38:43 +0200

    Fixed particles

--- Assets/Scripts/Camera Scripts/CameraShake.cs
+++ Assets/Scripts/Camera Scripts/CameraShake.cs
@@ -10,6 +10,15 @@ namespace UnityCore
         public class CameraShake : MonoBehaviour
         {
             public float delayBetweenCameraShake;
+            public ParticleSystem bricks;
+            public ParticleSystem dust;
+            public ParticleSystem cogwheel;
+
+            public float offsetFromPlayer;
+
+            Vector3 followPlayer;
+            GameObject player;
+
             CinemachineImpulseSource impulse;
             AudioController audioController;
             private bool shakeIsTrue;
@@ -18,15 +27,17 @@ namespace UnityCore
             {
                 audioController = GameObject.Find("AudioController").GetComponent<AudioController>();
                 impulse = transform.GetComponent<CinemachineImpulseSource>();
-
                 shakeIsTrue = true;
+                player = GameObject.FindGameObjectWithTag("Player");
             }
 
             // Update is called once per frame
             void Update()
             {
+
                 if (shakeIsTrue)
                 {
+                    followPlayer = player.transform.position + player.transform.up * offsetFromPlayer;
                     Invoke("shake", delayBetweenCameraShake);
                     shakeIsTrue = false;
                 }
@@ -34,10 +45,31 @@ namespace UnityCore
 
             void shake()
             {
+                bricks = Instantiate(bricks, followPlayer, transform.rotation);
+                cogwheel = Instantiate(cogwheel, followPlayer, transform.rotation);
+                dust = Instantiate(dust, followPlayer, transform.rotation);
                 impulse.GenerateImpulse();
                 shakeIsTrue = true;
                 audioController.PlayAudio(AudioType.SFX_BellSound, true);
+                activateParticles();
             }
+
+            void activateParticles()
+            {
+
+                bricks.Play();
+                cogwheel.Play();
+                dust.Play();
+                Invoke(nameof(stopParticles), 3);
+            }
+
+            void stopParticles()
+            {
+                bricks.Stop();
+                dust.Stop();
+                cogwheel.Stop();
+            }
+
         }
     }
 }

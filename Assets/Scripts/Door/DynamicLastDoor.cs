309c84ab3f1e05447420820e73b3ec75a26882bc
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2022-05-20 12:45:53 +0200

    comits

---
+++ Assets/Scripts/Door/DynamicLastDoor.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class DynamicLastDoor : MonoBehaviour
+{
+
+    Rigidbody rgbd;
+
+    public bool swiningDoor, openDoor;
+    bool holdDoorState, needVirtualCamera;
+
+    public int keysAdded;
+
+    public Animator anim;
+
+    public CinemachineVirtualCamera camera;
+
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody>();
+        holdDoorState = openDoor;
+        if (TryGetComponent<Animator>(out Animator animator))
+        {
+            anim = animator;
+        }
+
+        keysAdded = 0;
+    }
+
+    void Update()
+    {
+        if (swiningDoor)
+        {
+            if (openDoor)
+            {
+                rgbd.constraints = RigidbodyConstraints.None;
+            }
+            else
+            {
+                rgbd.constraints = RigidbodyConstraints.FreezeRotationY;
+            }
+            return;
+        }
+
+        if (holdDoorState == openDoor)
+            return;
+
+
+        if (openDoor)
+        {
+            DoorOpen();
+        }
+        else
+        {
+            DoorClose();
+        }
+        if (!openDoor)
+            return;
+
+        if (!needVirtualCamera)
+        {
+            cameraPrio();
+    needVirtualCamera = true;
+        }
+    }
+
+    void DoorOpen()
+{
+    anim.Play("door_open");
+}
+
+void DoorClose()
+{
+    anim.Play("door_close");
+}
+
+public void SetDoorOpen()
+{
+    anim.Play("door_open_static");
+    holdDoorState = openDoor;
+
+}
+
+public void SetDoorClosed()
+{
+    anim.Play("door_close_static");
+    holdDoorState = openDoor;
+}
+
+void cameraPrio()
+{
+    camera.Priority = 11;
+    Invoke(nameof(resetCameraPriority), 3);
+}
+
+void resetCameraPriority()
+{
+    camera.Priority = 0;
+}
+}
+
